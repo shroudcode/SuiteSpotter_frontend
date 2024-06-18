63ead1d506e2e5b728a14e30a5d09e654bf8fa77
varun <varunprashanthnaik@gmail.com>
2024-06-18 17:46:12 +0530

    frontend home page done

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -13,8 +13,8 @@ const Featured = () => {
           className="featuredImg"
         />
         <div className="featuredTitles">
-          <h1>Dublin</h1>
-          <h2>123 properties</h2>
+          <h1>Berlin</h1>
+          <h2>{data[0]} spot(s)</h2>
         </div>
       </div>
       
@@ -25,8 +25,8 @@ const Featured = () => {
           className="featuredImg"
         />
         <div className="featuredTitles">
-          <h1>Reno</h1>
-          <h2>533 properties</h2>
+          <h1>Madrid</h1>
+          <h2>{data[1]} spot(s)</h2>
         </div>
       </div>
       <div className="featuredItem">
@@ -36,12 +36,14 @@ const Featured = () => {
           className="featuredImg"
         />
         <div className="featuredTitles">
-          <h1>Austin</h1>
-          <h2>532 properties</h2>
+          <h1>London</h1>
+          <h2>{data[2]} spot(s)</h2>
         </div>
       </div>
     </div>
   );
 };
+
+
 
 export default Featured;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,13 @@
 import "./navbar.css"
+import { Link } from "react-router-dom"
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navContainer">
+        <Link to="/" className="link" style = {{color:"inherit", textDecoration:"none"}}>
         <span className="logo">SuiteSpotter</span>
+        </Link>
         <div className="navItems">
           <button className="navButton1">Get Started</button>
           <span className="divider">Already a user? </span>
